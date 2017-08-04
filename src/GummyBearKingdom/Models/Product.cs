54fceea6a9fdc54633890ac42ce7be41c0836b4c
davidrolfs <davidrolfs15@gmail.com>
2017-08-04 10:52:34 -0700

    create first view

---
+++ src/GummyBearKingdom/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummyBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Cost { get; set; }
+        public string Origin { get; set; }
+    }
+}
